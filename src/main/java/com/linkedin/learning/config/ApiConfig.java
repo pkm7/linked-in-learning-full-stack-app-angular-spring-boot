0dc24257bff5a0cbbcd9b543ad0a1a9d1b4a55e7
E104888 <E104888@AZ-687316.intrallianz.es>
2018-06-26 17:33:02 +0200

    :sparkles: new Endpoints

--- src/main/java/com/linkedin/learning/config/ApiConfig.java
+++ src/main/java/com/linkedin/learning/config/ApiConfig.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;;
 
 @Configuration
 public class ApiConfig {
 
 	@Bean
 	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		return objectMapper;
 	}
 	
 	
--- src/main/java/com/linkedin/learning/rest/ReservationResource.java
+++ src/main/java/com/linkedin/learning/rest/ReservationResource.java
@@ -6,12 +6,15 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.linkedin.learning.model.response.ReservationResponse;
+import com.linkedin.learning.request.ReservationRequest;
 
 @RestController
 @RequestMapping(ResourceConststants.ROOM_RESERVATION_V1)
@@ -30,4 +33,34 @@ public class ReservationResource {
 		
 		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
 	}
+	
+	
+	@RequestMapping(path="", method=RequestMethod.POST, 
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+			consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<ReservationResponse> createReservations(
+			@RequestBody
+			ReservationRequest reservationRequest){
+		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
+	}
+
+	
+	@RequestMapping(path="", method=RequestMethod.PUT, 
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+			consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<ReservationResponse> updateReservations(
+			@RequestBody
+			ReservationRequest reservationRequest){
+		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping(path="/{reservationId}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteReservation(
+			@PathVariable
+			long reservationId){
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
 }
